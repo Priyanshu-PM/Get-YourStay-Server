6ab36ae7e78c558fd92a275ef527875c495d1408
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2024-07-02 23:03:14 +0530

    token print

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
+import http from "http"; // Node's built-in HTTP server
+
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
@@ -9,21 +12,59 @@ import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
 const app = express();
+const server = http.createServer(app); // Create an HTTP server instance
 
-app.use(express.json()); 
-// const cors=require("cors");
-const corsOptions ={
-   origin:'http://localhost:5173', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(express.json());
+const corsOptions = {
+  origin: 'https://get-yourstay.vercel.app/', // Adjust to your frontend URL
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
+// Socket.io setup
+const io = new Server(server, {
+  cors: {
+    origin: "https://get-yourstay.vercel.app/",
+  },
+});
+
+let onlineUsers = [];
+
+const addUser = (userId, socketId) => {
+  const userExists = onlineUsers.find((user) => user.userId === userId);
+  if (!userExists) {
+    onlineUsers.push({ userId, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUsers.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+  });
+
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    io.to(receiver.socketId).emit("getMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
+// Routes
 app.use("/api/demo", (req, res) => {
-  res.json({"msg": "Server is running of localhost 8800, hello world"})
-})
+  res.json({ "msg": "Server is running on localhost 8800, hello world" });
+});
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
@@ -31,6 +72,9 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(8800, () => {
-  console.log("Server is running!");
+const PORT = process.env.PORT || 8800;
+
+server.listen(PORT, () => {
+  console.log(`Server and Socket.io listening on port ${PORT}`);
 });
+
